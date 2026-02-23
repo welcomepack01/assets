181afbfe4ccefec7195f0f84797f4580de1b755f
brandenpark <welcomepack@naver.com>
2026-02-23 22:48:56 +0900

    Auto Update: 2026-02-23 22:48:56.01

--- backup/run/theme/gameTheme.ts
+++
@@ -1,9 +0,0 @@
-
-
-import { GameTheme } from '../gameTypes';
-import { MarioTheme } from './mario/index';
-
-// This file now acts as the entry point for selecting the active theme.
-// To change the theme, import a different Theme object and assign it to CurrentTheme.
-
-export const CurrentTheme: GameTheme = MarioTheme;
